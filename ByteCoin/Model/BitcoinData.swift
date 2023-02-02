6ccdfc49cc8e0c4cb1a2eaf2e8cac2c395592903
oggysy <ogisoys@gmail.com>
2023-02-02 14:31:31 +0900

    initial commit

---
+++ ByteCoin/Model/BitcoinData.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinData.swift
+//  ByteCoin
+//
+//  Created by 小木曽佑介 on 2023/02/02.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct BitcoinData: Decodable{
+    let rate: Double
+    let asset_id_quote: String
+}
+
+
+
